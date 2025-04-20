7788607b15437ad94a1abb4c1a08c64361bd3484
asher-ma <asherhmangel@gmail.com>
2025-04-20 15:04:15 -0700

    Implemented water.php and fire.php

---
+++ water.php
@@ -0,0 +1,37 @@
+<?php include('header.php'); ?>
+<?php include('data/pokemon.php'); ?>
+
+<main>            
+    <table class="pokemon-table">
+        <thead>
+            <tr>
+                <th>Number</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Type(s)</th>
+                <th>Abilities</th>
+                <th>Height</th>
+                <th>Weight</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+        <?php
+        $waterPokemonList = getPokemonByType('Water');
+
+        foreach ($waterPokemonList as $pokemon): ?>
+            <tr>
+                <td><?= $pokemon['number'] ?></td>
+                <td><img src="<?= $pokemon['image'] ?>" alt="<?= $pokemon['name'] ?>" class="pokemon-img"></td>
+                <td><?= $pokemon['name'] ?></td>
+                <td><?= implode(', ', $pokemon['types']) ?></td>
+                <td><?= implode(', ', $pokemon['abilities']) ?></td>
+                <td><?= $pokemon['height'] ?></td>
+                <td><?= $pokemon['weight'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
+
+<?php include('footer.php'); ?>
